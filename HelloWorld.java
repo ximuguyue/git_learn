131e0736d12db84239a0ecc0b01f90c714b96e72
lishuaifei <lishuaifei@parim.net>
2016-12-23 09:51:26 +0800

    Update HelloWorld.java File add two line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld
 {
 	public static void main(String args[]){
 	System.out.println("Hello World!");
+	System.out.println("Hello ximuguyue!");
+	System.out.println("ximuguyue!");
 	}
 }
